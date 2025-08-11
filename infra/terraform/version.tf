0bf01d4427a21c66168a4424b657022d1ba279ad
Dhyey1811 <dhyeypatel9640@gmail.com>
2025-08-11 18:10:51 -0400

    change in pr

--- infra/terraform/version.tf
+++ infra/terraform/version.tf
@@ -1,9 +1,16 @@
 terraform {
   required_version = ">= 1.6.0"
   required_providers {
-    aws     = { source = "hashicorp/aws", version = ">= 5.50" }
-    archive = { source = "hashicorp/archive" }
-    random  = { source = "hashicorp/random" }
+    aws    = { source = "hashicorp/aws", version = ">= 5.50" }
+    archive= { source = "hashicorp/archive" }
+    random = { source = "hashicorp/random" }
   }
 }
-provider "aws" { region = var.aws_region }
+
+provider "aws" {
+  region                      = var.aws_region
+  skip_credentials_validation = var.ci
+  skip_metadata_api_check     = var.ci
+  skip_requesting_account_id  = var.ci
+
+}
